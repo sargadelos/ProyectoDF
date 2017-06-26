84e329f67b67193c54c438243b063b8a3abeb21a
sargadelos <campitosp@gmail.com>
2017-06-26 23:01:46 +0200

    Version con Spark session

--- nodoDF/src/main/scala-2.11/proyectoDF/cluster/nodo/nodoDataFederation.scala
+++ nodoDF/src/main/scala-2.11/proyectoDF/cluster/nodo/nodoDataFederation.scala
@@ -1,24 +1,25 @@
 package proyectoDF.cluster.nodo
 
 
-import java.net.NetworkInterface
+
 
 import language.postfixOps
 import scala.concurrent.Future
-import akka.actor.Actor
-import akka.actor.ActorRef
-import akka.actor.Terminated
+import akka.actor.{Actor, ActorPath, ActorRef, ActorSystem, Terminated}
 import akka.util.Timeout
+import akka.cluster.pubsub.DistributedPubSub
+import akka.cluster.pubsub.DistributedPubSubMediator.{Publish, SendToAll, Send, Subscribe}
 
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import akka.pattern.pipe
 import akka.pattern.ask
 import jdk.nashorn.internal.runtime.regexp.joni.Config
-import proyectoDF.cluster.mensajeria.{peticionDF, respuestaDF}
+import proyectoDF.cluster.mensajeria.{metaDataDF, peticionDF, respuestaDF}
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SQLContext
+import akka.cluster.client.{ClusterClient, ClusterClientSettings}
 
 import scala.collection.mutable.HashMap
 
@@ -26,9 +27,16 @@ class nodoDataFederation extends Actor {
 
   var contador = 0
 
-//  val conf = new SparkConf().setAppName("SparkNodoConexion").setMaster("spark://utad-virtual-machine:7077")//.set("spark.ui.port", "44040" )
-//  val sc = new SparkContext(conf)
+  // Variables para replicacion metada
+//  val initialContacts = Set(
+//    ActorPath.fromString("akka.tcp://ClusterDataFederation@192.168.1.20:2551/system/receptionist"))
+//  val settings = ClusterClientSettings(context.system)
+//    .withInitialContacts(initialContacts)
+//  val c = context.system.actorOf(ClusterClient.props(settings), "ClienteMetaData")
 
+
+  // activate the extension
+  val mediator = DistributedPubSub(context.system).mediator
 
   val spark = org.apache.spark.sql.SparkSession.builder
     .master("spark://df-machine-01:7077")
@@ -43,23 +51,33 @@ class nodoDataFederation extends Actor {
     .load("/home/utad/Descargas/airports.csv"); //.csv("csv/file/path") //spark 2.0 api
 
   // after registering as a table you will be able to run sql queries
-  df.registerTempTable("airports")
+  df.createOrReplaceTempView("airports")
 
 
 
   // create Spark context with Spark configuration
   def receive = {
     case job: peticionDF =>
-      println(s"Recibida peticion en DataFederation (): '${job.texto}'")
+      println(s"Recibida peticion en DataFederation (): '${job.text}'")
       contador += 1
 
       // Load our input data.
-      val resultado = spark.sql(s"select airport from airports where iata like '${job.texto}'")
+      val resultado = spark.sql(s"select airport from airports where iata like '${job.text}'")
 
       resultado.collect().foreach(t => sender() ! respuestaDF("Respuesta: "+t(0)))
 //      implicit val timeout = Timeout(1 seconds)
 
-      sender() ! respuestaDF(s"Fin de la Respuesta del DataFederation a: '${job.texto}'")
+//      sender() ! respuestaDF(s"Fin de la Respuesta del DataFederation a: '${job.text}'")
+      println ("Mandamos Metadata")
+      mediator ! SendToAll("/user/nodo", metaDataDF(job.text), allButSelf = true)
+
+    case job: metaDataDF =>
+      println(s"Metadata: ${job.text}")
+      if (sender == self)
+        println("Recibida metadata propio: no hacemos nada")
+      else
+        println("Recibida metadata ajeno: lo reenviamos como peticion")
+        self ! peticionDF (job.text)
 
   }
 }
